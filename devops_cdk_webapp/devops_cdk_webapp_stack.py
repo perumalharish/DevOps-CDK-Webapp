36efbc659b93b5108cf253fb2d8339e1029fa939
Harishreddy <Harishreddy205@gmailcom>
2026-05-05 20:24:35 -0500

    fix: add environment config, CloudWatch logging, ALB security groups

--- devops_cdk_webapp/devops_cdk_webapp_stack.py
+++ devops_cdk_webapp/devops_cdk_webapp_stack.py
@@ -7,6 +7,7 @@ from aws_cdk import (
     aws_ecs_patterns as ecs_patterns,
     aws_s3 as s3,
     aws_iam as iam,
+    aws_logs as logs,
 )
 from constructs import Construct
 
@@ -16,7 +17,7 @@ class DevopsCdkWebappStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs):
         super().__init__(scope, construct_id, **kwargs)
 
-        # VPC
+        # 2 AZs, private subnets go through NAT to reach internet
         vpc = ec2.Vpc(
             self,
             "WebVpc",
@@ -24,29 +25,46 @@ class DevopsCdkWebappStack(Stack):
             nat_gateways=1
         )
 
-        # S3 Bucket
+        # private bucket, access only via pre-signed URLs
         bucket = s3.Bucket(
             self,
             "WebBucket",
             block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
             encryption=s3.BucketEncryption.S3_MANAGED,
+            enforce_ssl=True,
             removal_policy=RemovalPolicy.DESTROY,
             auto_delete_objects=True,
         )
 
-        # ECS Cluster
         cluster = ecs.Cluster(self, "Cluster", vpc=vpc)
 
-        # IAM Role
+        # tasks need read/write on S3 and permission to push logs to CloudWatch
         task_role = iam.Role(
             self,
             "TaskRole",
             assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
         )
-
         bucket.grant_read_write(task_role)
+        task_role.add_managed_policy(
+            iam.ManagedPolicy.from_aws_managed_policy_name("CloudWatchAgentServerPolicy")
+        )
 
-        # Fargate Service + ALB
+        log_group = logs.LogGroup(
+            self,
+            "WebAppLogs",
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
+        # only ports 80 and 443 should reach the load balancer
+        alb_sg = ec2.SecurityGroup(
+            self,
+            "AlbSG",
+            vpc=vpc,
+            allow_all_outbound=True,
+        )
+        alb_sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(80))
+        alb_sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(443))
+
         fargate_service = ecs_patterns.ApplicationLoadBalancedFargateService(
             self,
             "FargateService",
@@ -55,6 +73,7 @@ class DevopsCdkWebappStack(Stack):
             memory_limit_mib=512,
             desired_count=2,
             public_load_balancer=True,
+            security_groups=[alb_sg],
             task_subnets=ec2.SubnetSelection(
                 subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS
             ),
@@ -62,40 +81,26 @@ class DevopsCdkWebappStack(Stack):
                 image=ecs.ContainerImage.from_asset("."),
                 container_port=80,
                 task_role=task_role,
+                log_driver=ecs.LogDrivers.aws_logs(
+                    stream_prefix="webapp",
+                    log_group=log_group,
+                ),
             ),
         )
 
-        # Auto Scaling
+        # scale between 2 and 4 tasks based on CPU
         scaling = fargate_service.service.auto_scale_task_count(
             min_capacity=2,
             max_capacity=4,
         )
-
         scaling.scale_on_cpu_utilization(
             "CpuScaling",
             target_utilization_percent=60,
         )
 
-        # Outputs
-        CfnOutput(
-            self,
-            "LoadBalancerDNS",
+        CfnOutput(self, "LoadBalancerDNS",
             value=fargate_service.load_balancer.load_balancer_dns_name,
         )
-
-        CfnOutput(
-            self,
-            "BucketName",
+        CfnOutput(self, "BucketName",
             value=bucket.bucket_name,
         )
-# CfnOutput(
-#     self,
-#     "LoadBalancerDNS",
-#     value=fargate_service.load_balancer.load_balancer_dns_name,
-# )
-
-# CfnOutput(
-#     self,
-#     "BucketName",
-#     value=bucket.bucket_name,
-# )
